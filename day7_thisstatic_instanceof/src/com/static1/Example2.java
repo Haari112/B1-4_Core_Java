194b449f62c28415073d4a770469a2d949d30aae
Anup <Anup@LAPTOP-LCUPKCAJ.mshome.net>
2022-04-07 15:36:49 +0530

    First Commit

---
+++ day7_thisstatic_instanceof/src/com/static1/Example2.java
@@ -0,0 +1,14 @@
+package com.static1;
+
+public class Example2 {
+	static void print()
+	{
+		System.out.println("Harish");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		print();
+	}
+
+}
