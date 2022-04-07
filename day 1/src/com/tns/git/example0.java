aa2bf9ec028cfb09926d32d3fa251e118f6795d2
Anup <Anup@LAPTOP-LCUPKCAJ.mshome.net>
2022-04-07 12:07:26 +0530

    First Commit

--- day 1/src/com/tns/git/example0.java
+++ day 1/src/com/tns/git/example0.java
@@ -4,6 +4,20 @@ public class example0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a = 10, b = 4;
+	    System.out.println("a + b = " + (a + b));
+
+	   
+	    System.out.println("a - b = " + (a - b));
+
+	    
+	    System.out.println("a * b = " + (a * b));
+
+	    
+	    System.out.println("a / b = " + (a / b));
+
+	   
+	    System.out.println("a % b = " + (a % b));
 
 	}
 
