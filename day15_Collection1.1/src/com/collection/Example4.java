5f72fbb53a5fcaac6ac8b20944558ee3b23693cf
ashrithagit <ashritha1ga18ec025@gmail.com>
2022-04-19 17:29:34 +0530

    First Commit

---
+++ day15_Collection1.1/src/com/collection/Example4.java
@@ -0,0 +1,16 @@
+
+package com.collection;
+
+import java.util.Vector;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector v=new Vector();
+		v.add("Harish");
+		v.add(11);
+		System.out.println(v);
+	}
+
+}
