2a1e6aa6954537c2009bc021f53c8b9f7174e80a
Anup <Anup@LAPTOP-LCUPKCAJ>
2022-05-17 00:24:47 +0530

    First Commit

--- day 1/src/com/framework/BankFactory.java
+++ day 1/src/com/framework/BankFactory.java
@@ -1,5 +1,7 @@
 package com.framework;
 
-public class BankFactory {
-
+public abstract class BankFactory {
+	public abstract SavingAcc getNewSavingAccount(int accNo, String accNm, float accBal,boolean isSalary);
+	public abstract CurrentAcc getNewCurrentAccount(int accNo, String accNm, float accBal,float creditLimit);
+	
 }
